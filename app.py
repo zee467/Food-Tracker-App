2b33df7b6af19c80e3a4e108d78929579729dd09
zee467 <wura.mahmud@gmail.com>
2023-07-06 20:21:02 +0100

    Implement database helper functions

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+import os
 
 app = Flask(__name__)
+
+database_path = os.getenv("DATABASE_PATH")
+
+def connect_db():
+    sql = sqlite3.connect("")
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
 
 @app.route("/")
 def index():
